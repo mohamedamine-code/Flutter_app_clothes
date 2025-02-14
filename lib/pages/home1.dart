4fd8e6d793dc797787c7f405b40fe69cac4149bd
mohamedamine-code <mohamedaminebensir09@gmail.com>
2025-02-15 00:19:55 +0100

    second final

--- lib/pages/home1.dart
+++ lib/pages/home1.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class home extends StatelessWidget {
@@ -11,81 +9,157 @@ class home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: const Color.fromARGB(182, 39, 39, 39),
-        body: Container(
-          padding: EdgeInsets.only(left: 20, right: 20),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.star,
-                color: const Color.fromARGB(255, 237, 121, 4),
-                size: 65,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Ideate",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontFamily: 'font',
-                    ),
-                  ),
-                  Text(
-                    "Store",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontFamily: 'font',
-                      color: Color.fromARGB(255, 237, 121, 4),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: 
-                  Text(
-                  "Be unique",
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: const Color.fromARGB(255, 249, 249, 249),
-                  ),
-                  ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: 
-                  Text(
-                  "With your own style",
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: const Color.fromARGB(200, 249, 249, 249),
-                  ),
-                  ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Container(
-                height: 398,
-                width: 284,
-                child: ClipRRect(
-                  child: Image.asset(
-                    "assets/img/pds5ja_a7a5dd6247a00231fe4bada00fd7b5fad0a950da.jpg",
-                  ),
-
+        backgroundColor: const Color.fromARGB(30, 30, 30, 1),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.star,
+                  color: const Color.fromARGB(255, 237, 121, 4),
+                  size: 65,
                 ),
-              )
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Ideate",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontFamily: 'font',
+                      ),
+                    ),
+                    Text(
+                      "Store",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontFamily: 'font',
+                        color: Color.fromARGB(244, 142, 72, 1),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Be unique",
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: const Color.fromARGB(255, 255, 250, 250),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "With your own style",
+                    style: TextStyle(
+                      fontSize: 35,
+                      color: const Color.fromARGB(203, 249, 249, 249),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(150),
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 255, 135, 7),
+                      width: 2, // Optional: Make the border more visible
+                    ),
+                    color: Colors
+                        .black, // Optional: Background color if the image has transparency
+                  ),
+                  height: 398,
+                  width: 284,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(
+                        42), // Slightly less to match inside border
+                    child: Image.asset(
+                      "assets/img/Mask Group.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color.fromARGB(255, 255, 135, 7),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Container(
+                      height: 15,
+                      width: 15,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(
+                    Color.fromARGB(255, 255, 135, 7),
+                  )),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home2');
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 352,
+                    height: 75,
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/pages/home2.dart
+++ lib/pages/home2.dart
@@ -1,10 +1,282 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Home2 extends StatelessWidget {
   const Home2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(241, 255, 255, 255),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0, left: 15, right: 15),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 62,
+                          height: 62,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          "assets/svg/Star 1.svg",
+                          width: 38,
+                          height: 37,
+                          fit: BoxFit.cover,
+                        )
+                      ],
+                    ),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 62,
+                          height: 62,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SvgPicture.asset(
+                          "assets/svg/Category-1.svg",
+                          width: 28,
+                          height: 26,
+                          fit: BoxFit.cover,
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "Be unique",
+                    style: TextStyle(
+                      fontSize: 52,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    "With your own style",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                Stack(children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: SizedBox(
+                      width: 230,
+                      height: 64,
+                      child: TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search),
+                            hintText: "Search your style",
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(20)),
+                            filled: true,
+                            fillColor: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 73,
+                      height: 64,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(22),
+                        color: Color.fromARGB(255, 255, 135, 7),
+                      ),
+                      child: SizedBox(
+                        child: SvgPicture.asset(
+                          "assets/svg/Slider.svg",
+                          fit: BoxFit.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "New Arrival",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      "See all",
+                      style: TextStyle(color: const Color.fromARGB(208, 0, 0, 0)),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 225,
+                        height: 287,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image.asset(
+                            "assets/img/image 15.png",
+                            width: 225,
+                            height: 287,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Container(
+                        height: 287,
+                        width: 225,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(30), // Coins arrondis
+                        ),
+                        child: ClipRRect(
+                          // Découpe l'image pour respecter le borderRadius
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image.asset(
+                            "assets/img/image 14.png",
+                            height: 287, // Taille forcée
+                            width: 225, // Taille forcée
+                            fit: BoxFit
+                                .cover, // Remplit le Container tout en gardant le ratio
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Container(
+                        height: 287,
+                        width: 225,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(30), // Coins arrondis
+                        ),
+                        child: ClipRRect(
+                          // Découpe l'image pour respecter le borderRadius
+                          borderRadius: BorderRadius.circular(30),
+                          child: Image.asset(
+                            "assets/img/image 21.png",
+                            height: 287, // Taille forcée
+                            width: 225, // Taille forcée
+                            fit: BoxFit
+                                .cover, // Remplit le Container tout en gardant le ratio
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Categories",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    Text(
+                      "See all",
+                      style: TextStyle(color: const Color.fromARGB(208, 0, 0, 0)),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(
+                          "assets/img/image 14.png",
+                          fit: BoxFit.cover,
+                          height: 200,
+                          width: 120,
+                        ),
+                      ),
+                      SizedBox(width: 15,),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(
+                          "assets/img/image 15.png",
+                          fit: BoxFit.cover,
+                          height: 200,
+                          width: 120,
+                        ),
+                      ),
+                    SizedBox(width: 15,),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(
+                          "assets/img/image 21.png",
+                          fit: BoxFit.cover,
+                          height: 200,
+                          width: 120,
+                        ),
+                      ),
+                    SizedBox(width: 15,),
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.asset(
+                          "assets/img/Mask Group.png",
+                          fit: BoxFit.cover,
+                          height: 200,
+                          width: 120,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
